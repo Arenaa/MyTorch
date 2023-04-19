53fd55e3861d8683af936fe2a834be9dc47938f9
mahsa <khoshnoodi.ma@gmail.com>
2023-04-19 23:50:51 +0330

    Complete sgd module

--- mytorch/optim/sgd.py
+++ mytorch/optim/sgd.py
@@ -19,13 +19,12 @@ class SGD:
         for i in range(self.L):
 
             if self.mu == 0:
-
-                self.l[i].W = None  # TODO
-                self.l[i].b = None  # TODO
+                self.l[i].W = self.l[i].W - self.lr * self.l[i].backward(0)
+                self.l[i].b = self.l[i].b - self.lr * self.l[i].backward()
 
             else:
 
-                self.v_W[i] = None  # TODO
-                self.v_b[i] = None  # TODO
-                self.l[i].W = None  # TODO
-                self.l[i].b = None  # TODO
+                self.v_W[i] = self.mu * self.v_W[i] + self.l[i].backward()
+                self.v_b[i] = self.mu * self.v_b[i] + self.l[i].backward()
+                self.l[i].W = self.l[i].W - self.lr * self.v_W[i]
+                self.l[i].b = self.l[i].b - self.lr * self.v_b[i]
